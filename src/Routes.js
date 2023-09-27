8e71271e7682854698344ee1d242f0e3d8fa6386
uzair1761 <ozairkhanan@gmail.com>
2023-09-28 02:48:01 +0500

    login page setup

---
+++ src/Routes.js
@@ -0,0 +1,7 @@
+import  LoginPage from "../src/pages/LoginPage.jsx"
+
+
+
+export{
+    LoginPage,
+}
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { useState } from 'react'
+import {AiOutlineEye,AiOutlineEyeInvisible} from "react-icons/ai"
+
+const Login = () => {
+
+  const [email ,setEmail] =useState("");
+  const [password ,setPassword] =useState("");
+  const [visibility , setVisibility] =useState(false)
+
+
+
+  
+  return (
+    <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12  sm:px-6 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+       <h2 className='mt-6 text-center text-3xl font-extrabold  text-gray-900'>
+         Login your account
+
+       </h2>
+
+      </div>
+      <div className='mt-8 sm:mx-auto sm:w-full  sm:max-w-md'>
+       <div className='bg-white py-8 px-4 shadow sm:rounded-lg  sm:px-10'>
+
+            <form className="space-y-6">
+            <div>
+
+
+              <label htmlFor="email"   className='block  text-sm font-medium  text-gray-700'
+              
+              
+              > Email Address</label>
+            </div>
+
+            <div className="mt-1 ">
+              <input type="email"   name="email"  autoComplete='email' required  value={email}  onChange={(e)=>setEmail(e.target.value)}  className='appearance-none  block w-full px-3 py-2 border border-gray-300 rounded-md shadow placeholder-gray-400 focus:outline-none focus:ring-blue-500  focus:border-blue-500 sm:text-sm'/>
+            </div>
+            <div>
+
+
+                  <label htmlFor="email"   className='block  text-sm font-medium  text-gray-700'
+
+
+                  > Password</label>
+                  </div>
+
+                  <div className="mt-1  relative">
+                  <input type={visibility ? "text " : "password"}   name="password"  autoComplete='current-password' required  value={password}  onChange={(e)=>setPassword(e.target.value)}  className='appearance-none  block w-full px-3 py-2 border border-gray-300 rounded-md shadow placeholder-gray-400 focus:outline-none focus:ring-blue-500  focus:border-blue-500 sm:text-sm'/>
+                 {
+                  visibility ?
+                  (<AiOutlineEye
+                    className='absolute right-2 top-2 cursor-pointer'
+                    size={25}
+                    onClick={()=>setVisibility(false)}
+                    />
+                    )
+                    :(
+                      <AiOutlineEyeInvisible
+                 className='absolute right-2 top-2 cursor-pointer'
+                 size={25}
+                 onClick={()=>setVisibility(true)}
+                 />
+                 
+                    )
+                 }
+                  </div>  
+                 
+
+
+
+            </form>
+       </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
